f2ef7b4c1e1446157f522cb2a41c7b2979c134e9
peven <peter.even@gmail.com>
2021-12-03 17:52:23 +0100

    aoc2021-d2p2

--- src/main/kotlin/day02/day02.kt
+++ src/main/kotlin/day02/day02.kt
@@ -1,25 +1,28 @@
 import java.io.File
 
 private fun getPair(input: List<String>): Pair<String,Int> = Pair(input.first(),input.last().toInt())
-
 private fun readCommands(filePath:String):List<Pair<String,Int>> = File(filePath).readLines().map { getPair(it.split(" ")) }
 
 
 fun main() {
     val instructions = readCommands(".\\src\\main\\kotlin\\day02\\input.txt")
 
-    var distance = 0
-    var depth = 0
+    var x = 0
+    var y = 0
+    var aim=0
 
     instructions.forEach {
         when (it.first) {
-            "forward" -> distance += it.second
-            "down" -> depth += it.second
-            "up" -> depth -= it.second
+            "forward" ->{
+                x += it.second
+                y += aim*it.second
+            }
+            "down" -> aim += it.second
+            "up" -> aim -= it.second
         }
     }
 
-    println("distance=$distance & depth=$depth")
-    println("result=${distance*depth}")
+    println("h-position=$x & depth=$y")
+    println("result=${x*y}")
 
 }
