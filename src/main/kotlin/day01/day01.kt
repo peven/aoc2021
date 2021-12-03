d968cc4a4448d033067306527df8d3cb51666d37
peven <peter.even@gmail.com>
2021-12-03 15:40:27 +0100

    aoc2021-d1p2 first attempt (wrong answer)

--- src/main/kotlin/day01/day01.kt
+++ src/main/kotlin/day01/day01.kt
@@ -1,21 +1,28 @@
 import java.io.File
+import kotlin.collections.ArrayDeque
 
 fun main() {
     val depthMeasurements = File(".\\src\\main\\kotlin\\day01\\input.txt").readLines().map { it.toInt()}
-    var previous = 0
+    val slidingWindow: ArrayDeque<Int> =  ArrayDeque(3)
     var count=0
+    var previousWindow = 0
 
     depthMeasurements.forEach { value ->
+        slidingWindow.add(value)
+        println(slidingWindow)
         when {
-            previous==0 -> println("N/A - no previous measurement")
-            value-previous>0 -> {
+            slidingWindow.count()<3 -> println("N/A - no previous measurement")
+            slidingWindow.sum()-previousWindow>0 -> {
                 println("increased")
                 count++
             }
             else -> println("decreased")
         }
-        previous = value
+        previousWindow = slidingWindow.sum()
+        if (slidingWindow.count()==3)
+            slidingWindow.removeFirst()
     }
 
     println("$count measurements are larger than the previous one")
 }
+
